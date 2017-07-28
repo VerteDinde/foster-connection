98b634a0c858c403fb17487f99aa61d393df03e8
Keeley Hammond <keeleymhammond@gmail.com>
2017-07-27 17:48:53 -0700

    Cover image styled

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,28 +1,37 @@
 import React, { Component } from 'react';
 import styled from 'styled-components';
 import { H1 } from '../Styles/utils';
-import headerImage from '../img/gongfu-setup.jpg';
+import backgroundImage from '../img/gongfu-setup.jpg';
 import List from './List';
 
-const HeaderImage = styled.div`
-  height: 100vh;
+const BackgroundImage = styled.div`
+  height: 80vh;
   width: 100vw;
-  background: url(${headerImage});
+  background: url(${backgroundImage});
   background-size: cover;
   background-repeat: no-repeat;
+`;
+
+const HeaderContainer = styled.div`
+  height: 80vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
 `;
 
 class Main extends Component {
   render() {
     const { user, locations } = this.props;
-    
+
     return (
-      <HeaderImage>
-        <div>
-          <H1>Gong Fu Tea</H1>
-          <List locations={locations}/>
-        </div>
-      </HeaderImage>
+      <BackgroundImage>
+        <HeaderContainer>
+          <H1>Gong Fu Tea <span>👍</span></H1>
+          <p>Welcome to Gong Fu Tea: Taiwanese Oolongs Only</p>
+        </HeaderContainer>
+        <List locations={locations} />
+      </BackgroundImage>
     );
   }
 }
