6adf9eab40a987fdd59ea1237c37c4c314ec2220
nicky evers <livelyevers@gmail.com>
2017-08-03 15:43:44 -0700

    Add tea images and inject google map with google-maps-react

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,3 +1,36 @@
-import React from 'react';
+import React, { Component } from 'react';
 import styled from 'styled-components';
 
+import { Map, InfoWindow, Marker, GoogleApiWrapper } from 'google-maps-react';
+
+// TODO: add function for places API (autocomplete!) 
+// https://www.npmjs.com/package/google-maps-react
+
+export class MapContainer extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      selectedPlace: 'something'
+    };
+  }
+  render() {
+    return (
+      <Map google={this.props.google} zoom={14}>
+
+        <Marker onClick={this.onMarkerClick}
+          name={'Current location'} />
+
+         <InfoWindow onClose={this.onInfoWindowClose}>
+          <div>
+            <h1>{this.state.selectedPlace.name}</h1>
+          </div>
+        </InfoWindow> 
+      </Map>
+    );
+  }
+}
+
+export default GoogleApiWrapper({
+  apiKey: ('')
+})(MapContainer);
