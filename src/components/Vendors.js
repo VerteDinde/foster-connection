25a96fbcb29d580ce2f416f9e18e4c5c594db1ff
Keeley Hammond <keeleymhammond@gmail.com>
2017-08-15 14:15:18 -0700

    Vendors added and generated beta

--- src/components/Vendors.js
+++ src/components/Vendors.js
@@ -1,57 +1,52 @@
 import React, { Component } from 'react';
+import styled from 'styled-components';
+import backgroundImage from '../assets/img/gongfu-setup.jpg';
 import vendors from '../data/vendors';
-import avatarPhoto from '../assets/img/teacup.jpg';
-
-import { List, ListItem } from 'material-ui/List';
+import { List } from 'material-ui/List';
 import Divider from 'material-ui/Divider';
-import Subheader from 'material-ui/Subheader';
-import Avatar from 'material-ui/Avatar';
-import { grey400, darkBlack } from 'material-ui/styles/colors';
-import IconButton from 'material-ui/IconButton';
-import MoreVertIcon from 'material-ui/svg-icons/navigation/more-vert';
-import IconMenu from 'material-ui/IconMenu';
-import MenuItem from 'material-ui/MenuItem';
+import VendorCard from './VendorCard';
 
-const iconButtonElement = (
-  <IconButton
-    touch={true}
-    tooltip="more"
-    tooltipPosition="bottom-left"
-  >
-    <MoreVertIcon color={grey400} />
-  </IconButton>
-);
+const BackgroundImage = styled.div`
+height: 30vh;
+width: 100vw;
+background: url(${backgroundImage});
+background-size: cover;
+background-repeat: no-repeat;
+`;
 
-const rightIconMenu = (
-  <IconMenu iconButtonElement={iconButtonElement}>
-    <MenuItem>Email</MenuItem>
-    <MenuItem>Website</MenuItem>
-  </IconMenu>
-);
+const HeaderContainer = styled.div`
+height: 30vh;
+display: flex;
+justify-content: center;
+align-items: center;
+flex-direction: column;
+color: white;
+`;
+
+const Title = styled.h1`
+font-weight: 400;
+font-size: 5em;
+`;
 
 class Vendors extends Component {
   render() {
     return (
-      <List style={{ width: '75%', marginLeft: '10%' }}>
-        <Subheader>Vendor List</Subheader>
-        {vendors.map(vendor => {
-          return <div>
-            <ListItem
-              leftAvatar={<Avatar src={avatarPhoto} />}
-              rightIconButton={rightIconMenu}
-              primaryText={vendor.name}
-              secondaryText={
-                <p>
-                  <span style={{ color: darkBlack }}>{vendor.website}</span> {vendor.description} -- {vendor.category}
-                </p>
-              }
-              secondaryTextLines={2}
-            />
-            <Divider inset={true} />
-          </div>;
-        })
-        };
+      <div>
+        <BackgroundImage>
+          <HeaderContainer>
+            <Title>Vendor List</Title>
+          </HeaderContainer>
+        </BackgroundImage>
+        <List style={{ width: '75%', marginLeft: '10%' }}>
+          {vendors.map(vendor => {
+            return <div>
+              <VendorCard vendor={vendor} />
+              <Divider inset={true} />
+            </div>;
+          })
+          };
       </List>
+      </div>
     );
   }
 
