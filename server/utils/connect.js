e0667fb820414fd1c5f5116cae454cca20e2ce92
nicky evers <livelyevers@gmail.com>
2017-10-25 14:13:30 -0700

    Comment out db connection temporarily

--- server/utils/connect.js
+++ server/utils/connect.js
@@ -1,25 +1,25 @@
-/*eslint-disable*/
-const mongoose = require('mongoose');
-mongoose.Promise = Promise;
+// /*eslint-disable*/
+// const mongoose = require('mongoose');
+// mongoose.Promise = Promise;
 
-const DB_URI = process.env.MONGODB_URI || 'mongodb://localhost:27017/gongfucha-v2';
-mongoose.connect(DB_URI);
+// const DB_URI = process.env.MONGODB_URI || 'mongodb://localhost:27017/gongfucha-v2';
+// mongoose.connect(DB_URI);
 
-mongoose.connection.on('connected', () => {
-  console.log('Mongoose default connection open at ' + DB_URI);
-});
+// mongoose.connection.on('connected', () => {
+//   console.log('Mongoose default connection open at ' + DB_URI);
+// });
 
-mongoose.connection.on('error', function (err) {
-  console.log('Mongoose default connection error:' + err);
-});
+// mongoose.connection.on('error', function (err) {
+//   console.log('Mongoose default connection error:' + err);
+// });
 
-mongoose.connection.on('disconnected', function () {
-  console.log('Mongoose default connection disconnected');
-});
+// mongoose.connection.on('disconnected', function () {
+//   console.log('Mongoose default connection disconnected');
+// });
 
-process.on('SIGNIT', function () {
-  mongoose.connection.close('disconnected', function () {
-    console.log('Mongoose default connection disconnected through app terminal');
-    process.exit(0);
-  });
-});
+// process.on('SIGNIT', function () {
+//   mongoose.connection.close('disconnected', function () {
+//     console.log('Mongoose default connection disconnected through app terminal');
+//     process.exit(0);
+//   });
+// });
