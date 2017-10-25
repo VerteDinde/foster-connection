ab046d4e0b23be0f5c6e9adcf9c2c4e7cf2fa747
nicky evers <livelyevers@gmail.com>
2017-10-25 14:13:15 -0700

    Write basic autocomplete route

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,8 +1,28 @@
 const express = require('express');
 const router = express.Router();
+const superAgent = require('superagent');
+// const app = express();
 
-router.get('/message', function(req, res, next) {
-  res.json('Welcome to React');
+router.get('/autocomplete', (request, response) => {
+  console.log(request, 'testing');
+});
+
+router.post('/autocomplete/:place', (request, response) => {
+  const place = request.body;
+  console.log(`  
+  
+  ${request}
+  
+  `);
+  superAgent
+    .get(
+      `https://maps.googleapis.com/maps/api/place/autocomplete/json?input=${place}&key=${process.env.REACT_API_GOOGLE_API_KEY}`
+    )
+    .then(res => {
+      console.log('were here');
+      res.send(res);
+    })
+    .catch(err => console.log(err));
 });
 
 module.exports = router;
